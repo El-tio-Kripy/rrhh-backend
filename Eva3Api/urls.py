34b8c66ced2cd17024bb4164577da889a6c9ab32
El-tio-Kripy <j.lopez98000@gmail.com>
2025-12-08 11:29:10 -0300

    Aplicar .gitignore correctamente

--- Eva3Api/urls.py
+++ Eva3Api/urls.py
@@ -1,11 +1,7 @@
 from django.urls import path
 
-<<<<<<< codex/implement-drf-view-for-trabajador-by-rut
 from .views import TrabajadorDetailView
 
 urlpatterns = [
     path("trabajador/<str:rut>/", TrabajadorDetailView.as_view(), name="trabajador-detail"),
 ]
-=======
-urlpatterns = []
->>>>>>> master
